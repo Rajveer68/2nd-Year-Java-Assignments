5f3fa80f92db061685a5c59f15b50502b16dbdb6
Rajveer Singh Saini <rajveersinghsaini56@gmail.com>
2025-10-10 11:12:03 +0100

    Final Stage 3

--- untitled/src/Lab2/ShapesMain.java
+++ untitled/src/Lab2/ShapesMain.java
@@ -13,30 +13,30 @@ public class ShapesMain {
         // Test 1: Function calls to all the Functions in Vector Class
         System.out.println("Testing Vector class functions");
 
-        System.out.println("");
+        System.out.println("Printing Vector 1:");
         v1.printVector();
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Printing Vector 2:");
         v2.printVector();
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Adding previous 2 Vectors");
         Vector sum = v1.add(v2);
         sum.printVector();
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Subtracting previous 2 Vectors");
         Vector sub = v1.subtract(v2);
         sub.printVector();
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Scaling Vector 1");
         Vector v5 = v1.scale(4);
         v5.printVector();
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Scaling Vector 2");
         Vector v6 = v2.scale(5);
         v6.printVector();
         System.out.println();
@@ -57,7 +57,7 @@ public class ShapesMain {
         Vector p = new Vector(2,4);
         System.out.println("Does rectangle contains point (2,4)? : " + rect.contains(p));
 
-        System.out.println("");
+        System.out.println("Calling All the functions from Rectangle Class at once with printing both the vectors first:");
         rect.printRectangle();
         System.out.println();
 
@@ -71,19 +71,19 @@ public class ShapesMain {
         System.out.println("Testing Circle class functions");
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Printing the centre coordinates of our circle");
         System.out.println(c1);
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Calculating Area of the given Circle");
         System.out.println("Area: " + c1.getArea());
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Calculating Diameter of the circle");
         System.out.println("Diameter: " + c1.getDiameter());
         System.out.println();
 
-        System.out.println("");
+        System.out.println("Getting the coordinates for smallest rectangle that fully encloses the circle and which is also parallel to axes:");
         System.out.println(c1.boundingBox());
         System.out.println();
 
