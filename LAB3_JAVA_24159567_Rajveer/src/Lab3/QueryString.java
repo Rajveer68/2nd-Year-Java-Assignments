f38c0db3c54779238365256a0e86134b1dd2dde7
Rajveer Singh Saini <rajveersinghsaini56@gmail.com>
2025-10-14 16:01:21 +0100

    LAB 3 completed

--- LAB3_JAVA_24159567_Rajveer/src/Lab3/QueryString.java
+++ LAB3_JAVA_24159567_Rajveer/src/Lab3/QueryString.java
@@ -1,5 +1,4 @@
 package Lab3;
-
 /*
  * Represents a query string, such as "foo=bar&baz=1234".
  * See https://en.wikipedia.org/wiki/Query_string
@@ -16,16 +15,31 @@ class QueryString {
      * or null if the parameter does not exist.
      */
     String getParameter(String name) {
-        // stub method
-        return null;
+        int offset = getParameterOffset(name);
+        if (offset == -1) {
+            return null;
+        }
+
+        // get the position after the next '=' symbol
+        int begin = data.indexOf('=', offset) + 1;
+
+        // get the position of the next '&' symbol
+        int end = data.indexOf('&', begin);
+        if (end == -1) {
+            // if there is no '&' symbol, this is the last parameter,
+            // so read to the end of the string
+            end = data.length();
+        }
+
+        String value = data.substring(begin, end);
+        return decode(value);
     }
 
     /*
      * Determines whether a parameter exists in the query string.
      */
     boolean hasParameter(String name) {
-        // stub method
-        return false;
+        return getParameterOffset(name) >= 0;
     }
 
     /*
@@ -33,8 +47,17 @@ class QueryString {
      * If the parameter does not exist, -1 is returned.
      */
     int getParameterOffset(String name) {
-        // stub method
-        return 0;
+        if (data.startsWith(name + "=")) {
+            // parameter appears at the start of the query string
+            return 0;
+        }
+
+        int offset = data.indexOf('&' + name + '=');
+        if (offset != -1) {
+            // the parameter exists, so skip the '&' symbol
+            offset++;
+        }
+        return offset;
     }
 
     /*
@@ -42,7 +65,23 @@ class QueryString {
      * See https://en.wikipedia.org/wiki/Percent-encoding
      */
     static String decode(String s) {
-        // stub method
-        return null;
+        String result = "";
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (c == '%') {
+                // decode two hexadecimal digits after the % symbol
+                String hex = s.substring(i + 1, i + 3);
+                int charCode = Integer.parseInt(hex, 16);
+                c = (char) charCode;
+                i += 2; // skip two more characters
+            } else if (c == '+') {
+                // '+' symbol is decoded to a space
+                c = ' ';
+            }
+            result += c;
+            i++;
+        }
+        return result;
     }
 }
--- LAB3_JAVA_24159567_Rajveer/src/Lab3/QueryStringTesting.java
+++ LAB3_JAVA_24159567_Rajveer/src/Lab3/QueryStringTesting.java
@@ -1,6 +1,65 @@
 package Lab3;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class QueryStringTesting {
+
+    @Test
+    public void testGetParameters(){
+        QueryString QT = new QueryString("name=Alice&age=19&country=UK"); //Query Parameter
+        assertEquals("Alice", QT.getParameter("name"));
+        assertEquals("19", QT.getParameter("age"));
+        assertEquals("UK", QT.getParameter("country"));
+        assertNull(QT.getParameter("occupation"));
+        assertNull(QT.getParameter("Alice"));
+        assertNull(QT.getParameter("&"));
+
+    }
+
+    @Test
+    public void testHasParameters(){
+        QueryString QT = new QueryString("name=Alice&age=19&country=UK");
+        assertTrue(QT.hasParameter("name"));
+        assertTrue(QT.hasParameter("age"));
+        assertTrue(QT.hasParameter("country"));
+        assertFalse(QT.hasParameter("occupation"));
+        assertFalse(QT.hasParameter("Alice"));
+        assertFalse(QT.hasParameter("&"));
+
+    }
+
+    @Test
+    public void testGetParameterOffset(){
+        QueryString QT = new QueryString("name=Alice&age=19&country=UK");
+
+        assertEquals(0, QT.getParameterOffset("name"));
+        assertEquals(11,QT.getParameterOffset("age"));
+        assertEquals(18,QT.getParameterOffset("country"));
+        assertEquals(-1, QT.getParameterOffset("occupations"));
+        assertEquals(-1,QT.getParameterOffset("Alice"));
+        assertEquals(-1,QT.getParameterOffset("&"));
+        assertTrue(QT.getParameterOffset("age") > 0);
+
+    }
+
+    @Test
+    public void testStringDecoder(){
+
+        assertEquals("Hello, world!", QueryString.decode("Hello%2C+world%21")); //URL Encoded String
+        assertEquals("I'll be a CEO!", QueryString.decode("I'll+be+a+CEO%21"));
+
+
+    }
+
+    @Test
+    public void testEmptyQuery() {
+        QueryString qs = new QueryString("");
+        assertFalse(qs.hasParameter("name"));
+        assertNull(qs.getParameter("name"));
+        assertEquals(-1, qs.getParameterOffset("name"));
+    }
 
 
 }
